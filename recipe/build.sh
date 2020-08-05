85dc56cba2bea9ddd93309082ac73f92c516a5ac
Jason Boutte <boutte.jason@gmail.com>
2020-08-04 17:34:50 -0700

    Updates recipe for 20.1.4 on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
-set -ex
 
-osname=`uname`
+meson builddir/ \
+  -Dbuildtype=release \
+  -Dprefix=$PREFIX \
+  -Dlibdir=lib \
+  -Dplatforms=x11 \
+  -Dosmesa=gallium \
+  -Dosmesa-bits=8 \
+  -Dgallium-drivers=swrast \
+  -Ddri-drivers=[] \
+  -Dvulkan-drivers=[] \
+  -Dglx=xlib \
+  -Dgbm=false \
+  -Degl=false \
+  -Dllvm=true \
+  -Dshared-llvm=false
 
-export CFLAGS="-Wno-implicit-function-declaration ${CFLAGS} -std=c11 -Wno-implicit-function-declaration"
-echo $CFLAGS
+ninja -C builddir/ -j ${CPU_COUNT}
 
-./configure \
-    --prefix=$PREFIX \
-    --disable-gles1 \
-    --disable-gles2 \
-    --disable-va \
-    --disable-gbm \
-    --disable-xvmc \
-    --disable-vdpau \
-    --enable-shared-glapi \
-    --enable-texture-float \
-    --disable-dri \
-    --with-dri-drivers= \
-    --with-gallium-drivers=swrast \
-    --disable-egl \
-    --with-egl-platforms= \
-    --enable-gallium-osmesa \
-    --disable-glx \
-    --enable-llvm \
-    --disable-llvm-shared-libs \
-    --with-osmesa-bits=32
-
-make -j${CPU_COUNT}
-make install
-
+ninja -C builddir/ install
